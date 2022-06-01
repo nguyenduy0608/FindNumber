7240052ef0d6fbe154ca3024d6776c27089363b4
nguyenduy0608 <105186077+nguyenduy0608@users.noreply.github.com>
2022-06-01 11:00:12 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import {useState,useRef} from 'react'
+import {useState,useRef, Suspense} from 'react'
 import axios from 'axios'
+import { useTranslation, Trans } from 'react-i18next';
 
+ const lngs = {
+  vi: {nativeName : 'Vi'},
+  en :{nativeName : 'En'}
+}
 function App() {
+  const { t ,i18n} = useTranslation();
   const [link, setLink] = useState('')
   const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState('');
+  const [data, setData] = useState(t('description.part18'));
   const [error, setError] = useState(null);
+  const [saves,setSave] = useState([])
   const InputRef = useRef()
   const setSearch = ()=>{
 
@@ -14,12 +21,14 @@ function App() {
   const getData = async () => {
     setIsLoading(true);
     setLink('')
+    
     try {
       const res = await axios.get(
         `http://dev.winds.vn:22522/users?id=${link}`
       );
       setData(res.data.data.phone);
       console.log(data)
+      saves.push(data)
     } catch (error) {
       setError(error);
      // console.log(error)
@@ -27,55 +36,72 @@ function App() {
       setIsLoading(false);
     }
     InputRef.current.focus()
+   
     // console.log(res.data);
   }
   return (
     <div className='main'> 
     <div className='header'>
+    <div> 
+      {/* nút chuyển đổi ngôn ngữ */}
+        {Object.keys(lngs).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }}
+             type="submit" onClick={() => i18n.changeLanguage(lng)}>
+            {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
       <h1 style ={{ textAlign : 'center',
       color : 'blue'
-    }}> FIND PHONE NUMBER</h1>
+    }}> {t('description.part12')}</h1>
     </div>
     <div className='Content'> 
-      <b style  ={{}}> Facebook profile link : </b>
+      <b style  ={{}}> {t('description.part13')} </b>
       <br/>
       <input   
       className='set-content'
       ref = {InputRef}
-      placeholder = 'xin mời nhập link...' value={link} name = "url_facebook"
+      placeholder = {t('description.part15')} value={link} name = "url_facebook"
       autoComplete="off"
       onChange={e => setLink(e.target.value)}
       />
       <br/>
       <button onClick={getData} 
-      className = "search-submit">Find number</button>
-      <div> Phone number : {data}</div>
+      className = "search-submit">{t('description.part14')}</button>
+      <div>  
+         {/* <p> Phone number :</p>  */}
+         { data ? <p> {t('description.part16')} {data } </p> : <p>{t('description.part17')}</p> }
+      
+      </div>
+      {/* <ul> 
+        {saves.map(save => (
+          <li> {save}</li>
+        ))}
+        </ul>    */}
+
     </div>
     <div className = 'Tutorial'>
-      <h1> What is this tool?</h1>
-      <p> This tool called "Find Facebook ID" provides an easy and fast way to 
-        find the Facebook ID for your profile or a group or a page.
-         There is also other information such as User name, Name, Type, ...</p>
+      <h1> { t('description.part1')}</h1>
+      <p> {t('description.part2')}</p>
          <h4> Example</h4>
          <ul style = {{ listStyleType: 'none'}}> 
-           <li>- For Profile type: Profile picture, Id, User name, Name, Old page id (if converted from classic page) </li>
-           <li>- For Page type: Page picture, Id, User name, Name, Likers count, Description.</li>
-           <li>- For Group type: Group picture, Id, User name, Name, Members count, Privacy info.</li>
+           <li>{t('description.part3')}</li>
+           <li>{t('description.part4')}</li>
+           <li>{t('description.part5')}</li>
          </ul>
-         <h1> How to use?</h1>
-         <p> This tool accepts Facebook URL, see the following example URL formats below:</p>
-         <ul> 
+         <h1> { t('description.part6')}</h1>
+         <p> { t('description.part7')}</p>
+         <ul >
            <li >https://www.facebook.com/zuck </li>
            <li>https://m.facebook.com/MTP.Fan</li>
            <li>https://m.facebook.com/groups/FindidfbCom</li>
            <li>https://www.facebook.com/profile.php?id=100012362696847</li>
          </ul>
-         <p>Put your Facebook URL and click the "Find numeric ID →" button, or simply press enter. The numeric ID will appear in the green box.</p>
-         <h1> Why use this tool?</h1>
-         <p> 1. Numeric Facebook IDs are needed when retrieving some data from Facebook.</p>
-         <p> 2. You will kinda know your place in Facebook history. For example, 
-           we will enter the URL https://www.facebook.com/zuck on the text box below,
-            the extracted ID will show "4" which means he is one of the earliest users or records in Facebook database.</p>
+         <p>{ t('description.part8')}</p>
+         <h1>{ t('description.part9')}</h1>
+         <p> { t('description.part10')}</p>
+         <p> { t('description.part11')}</p>
+         
     </div>
       <div className='Footer'>
       © 2020 • Powered by 
@@ -88,5 +114,5 @@ function App() {
     
   );
 }
-
 export default App;
+
